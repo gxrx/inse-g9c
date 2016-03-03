b116ce5f68b41b671dacb228a08a554cd5267aa9
WaltersD96 <up725137@myport.ac.uk>
2016-03-03 09:07:55 +0000

    Update Project.java
    Added:
    addEvent()
    checkDuplicate()

--- Event_Handling/Project.java
+++ Event_Handling/Project.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 import java.util.Calendar;
 
@@ -12,7 +13,7 @@ public class Project {
     public Project(String na, int[] st, int[] en) {
         eStart = Calendar.getInstance();
         eEnd = Calendar.getInstance();
-        eStart.set(st[0], st[1], st[2]); 
+        eStart.set(st[0], st[1], st[2]);
         eEnd.set(en[0], en[1], en[2]);
         name = na;
 
@@ -34,10 +35,30 @@ public class Project {
     public int getSize() {
         return eventList.size();
     }
-    
-    public ArrayList<Event> getEventList()
-    {
+
+    public ArrayList<Event> getEventList() {
         return eventList;
     }
 
+    public boolean addEvent(double inputID, Event event) {
+        if (checkDuplicate(inputID) == true)
+        {
+           eventList.add(event); 
+        }
+        return false;
+
+    }
+
+    //checks for a duplcate Event ID. returns false if there is. 
+    public boolean checkDuplicate(double inputID) {
+        String stringInputID = String.valueOf(inputID);
+        String eventlistID = "";
+        for (Event e : eventList) {
+            eventlistID = String.valueOf(e.getID());
+            if (stringInputID.equals(eventlistID)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
